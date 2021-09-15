8cd07ba6c1e78b0139bd12849e42ae95366c97bc
Maksim Shitsko <maksim.shitsko@itechart-group.com>
2021-09-15 17:08:54 +0300

    add project page

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_095506) do
+ActiveRecord::Schema.define(version: 2021_09_15_123150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2021_09_10_095506) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "project_id"
+    t.text "description"
+    t.string "image_url"
     t.index ["project_id"], name: "index_projects_on_project_id"
   end
 
